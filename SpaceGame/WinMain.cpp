bfb762a4c4fdd5e3c74658b4f5282dc859f7bf15
stzups <lemonpickles1558@gmail.com>
2020-05-14 16:27:54 -0400

    fixed window close

--- SpaceGame/WinMain.cpp
+++ SpaceGame/WinMain.cpp
@@ -1,5 +1,16 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(0);
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
@@ -12,7 +23,7 @@ int CALLBACK WinMain(
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -37,10 +48,19 @@ int CALLBACK WinMain(
 	ShowWindow(hWnd, SW_SHOW);
 
 	MSG msg;
-	while (GetMessage(&msg, nullptr, 0, 0) > 0)
+	BOOL gResult;
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0) > 0))
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
-	return 0;
+
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
 }
