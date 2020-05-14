e393abef5eab9f22591b436f079408817cca4a3d
stzups <lemonpickles1558@gmail.com>
2020-05-14 16:09:01 -0400

    actual initial commit

---
+++ SpaceGame/WinMain.cpp
@@ -0,0 +1,38 @@
+#include <Windows.h>
+
+int CALLBACK WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR     lpCmdLine,
+	int       nCmdShow)
+{
+	const auto pClassName = "something";
+
+	// window class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hIcon = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+
+	// window instance
+	HWND hWnd = CreateWindowEx(
+		0, pClassName,
+		"a new window",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr, nullptr, hInstance, nullptr
+	);
+	while (true);
+	return 0;
+}
