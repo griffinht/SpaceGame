edec6987dc38e268079714413df3851a49738502
stzups <lemonpickles1558@gmail.com>
2020-05-15 21:53:08 -0400

    add base exception class

---
+++ SpaceGame/EngineException.cpp
@@ -0,0 +1,39 @@
+#include "EngineException.h"
+
+#include <sstream>
+
+EngineException::EngineException(int line, const char* file) :
+	line(line),
+	file(file)
+{
+}
+
+const char* EngineException::what() const
+{
+	std::ostringstream oss;
+	oss << GetType() << " " << GetOriginString();
+	whatBuffer = oss.str();
+	return whatBuffer.c_str();
+}
+
+const char* EngineException::GetType() const
+{
+	return "Engine Exception";
+}
+
+int EngineException::GetLine() const
+{
+	return line;
+}
+
+const std::string& EngineException::GetFile() const
+{
+	return file;
+}
+
+std::string EngineException::GetOriginString() const
+{
+	std::ostringstream oss;
+	oss << "error occured in " << file << " at line " << line;
+	return oss.str();
+}
---
+++ SpaceGame/EngineException.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <exception>
+#include <string>
+
+class EngineException : std::exception
+{
+public:
+	EngineException(int line, const char* file);
+	const char* what() const override;
+	virtual const char* GetType() const;
+	int GetLine() const;
+	const std::string& GetFile() const;
+	std::string GetOriginString() const;
+private:
+	int line;
+	std::string file;
+protected:
+	mutable std::string whatBuffer;
+};
