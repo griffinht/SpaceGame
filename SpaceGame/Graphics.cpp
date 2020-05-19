c9c878c4dd1456c8fc98586a3934b78a15d1a3b1
stzups <lemonpickles1558@gmail.com>
2020-05-19 12:26:48 -0400

    fix swap effect

--- SpaceGame/Graphics.cpp
+++ SpaceGame/Graphics.cpp
@@ -249,6 +249,7 @@ void Graphics::CreateResources()
 		swapChainDesc.SampleDesc.Quality = 0;
 		swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
 		swapChainDesc.BufferCount = backBufferCount;
+		swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
 
 		DXGI_SWAP_CHAIN_FULLSCREEN_DESC fsSwapChainDesc = {};
 		fsSwapChainDesc.Windowed = TRUE;
